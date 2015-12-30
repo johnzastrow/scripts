1799396e4e4c1182402e2a7fb29a4770056b6d7c
John Zastrow <br8kwall@gmail.com>
2015-12-30 13:00:39 -0500

    Create NewUbuntuTODO.sh

---
+++ NewUbuntuTODO.sh
@@ -0,0 +1,6 @@
+# stuff to do to a new Ubuntu install
+# jcz 30-Dec-2015
+
+# install postgres, pgadmin, contribs, postgis, from the postgres apt repo
+# consider installing pgloader from same place to migrate data
+# install zip, unzip, nano, git, 
