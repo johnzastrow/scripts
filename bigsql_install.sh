1e533c51d6dc14fc8d056c12f5054332097d7df4
John Zastrow <br8kwall@gmail.com>
2018-01-09 14:23:16 -0500

    New changes

--- bigsql_install.sh
+++ bigsql_install.sh
@@ -3,7 +3,7 @@
 apt list --installed >> verbose_install_packages.txt
 dpkg -l | grep '^ii' | awk '{print $2 " "}' >> installed_packages_list.txt
 
-apt install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python rrdtool librrds-perl 
+apt install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python rrdtool librrds-perl python3 python-minimal curl 
 
 
 python -c "$(curl -fsSL https://s3.amazonaws.com/pgcentral/install.py)"
@@ -14,5 +14,25 @@ wget -O webmin.deb https://github.com/johnzastrow/scripts/raw/master/webmin_1.87
 dpkg -i webmin.deb
 
 
+bigsql/pgc list
+bigsql/pgc update
+bigsql/pgc list
+bigsql/pgc install pg10
+pgc install postgis24-pg10
+ 
+cd bigsql/pg10/
+bigsql/pgc start pg10
+psql
 
+source /home/jcz/bigsql/pg10/pg10.env
+
+psql -f bigsql/pg10/share/doc/postgresql/extension/create_postgis_sample_db.sql
+sudo apt install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python rrdtool librrds-perl openssh-server python3 python-minimal curl
+wget -O webmin.deb https://github.com/johnzastrow/scripts/raw/master/webmin_1.870_all.deb 
+ 
+sudo dpkg -i webmin.deb
+ifconfig
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
 
