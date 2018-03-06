1e5c0f152c72d1c056950c5514b897fb78e17c10
John Zastrow <br8kwall@gmail.com>
2018-03-05 20:34:55 -0500

    gfdg g
    df g

---
+++ scratch.py
@@ -0,0 +1,15 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+my_list[2] = 'a'
+print(my_list)
+my_list.append(7)
+my_list.append(8)
+print(my_list)
+my_list += [6, 89, 54]
+print(my_list)
+my_list += [34, 32523, 235, 32542]
+print(my_list)
+
+point = (2.0, 3.0)
+point.count()
+point.
